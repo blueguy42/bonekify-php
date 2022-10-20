b2fd75481d6bc863770de94178d81ebe47a7194a
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2022-10-20 12:09:10 +0700

    feat: add basic routing and docker config

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
 FROM php:8.0-apache
+RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+RUN apt-get update && apt-get upgrade -y
+RUN a2enmod rewrite
+
+ADD . /var/www/html
---
+++ app/core/App.php
@@ -0,0 +1,46 @@
+<?php 
+class App{
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params =[];
+    public function __construct(){
+        //url[0] adalah nama file controller, url[1] adalah nama method
+        $url = $this->parseURL();
+
+        //cek apakah ada file dengan nama tersebut, kalau ada jadikan nama controller
+        if(file_exists('../app/controllers/'.$url[0].'php')){
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/'.$this->controller.'.php';
+        $this->controller = new $this->controller;
+
+        //cek apakah terdapat method pada url dan method tersebut ada pada kelas controller. Bila ada timpa
+        if(isset($url[1])){
+            if(method_exists($this->controller,$url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        //ambil parameter bila ada
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        call_user_func_array([$this->controller,$this->method], $this->params);
+
+
+    }
+
+    public function parseURL(){
+        if( isset($_GET['url'])){
+            $url = rtrim($_GET['url'],'/');
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
+    }
+}
+?>
