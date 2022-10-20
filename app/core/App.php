37cc1c7e66515934ab7e64d2e6856d6fc01e0326
Saul Sayers <saulsayers@gmail.com>
2022-10-20 18:04:56 +0700

    feat: first view

--- app/core/App.php
+++ app/core/App.php
@@ -9,7 +9,7 @@ class App{
 
         //cek apakah ada file dengan nama tersebut, kalau ada jadikan nama controller
         if(isset($url[0])){
-            if(file_exists('../app/controllers/'.$url[0].'php')){
+            if(file_exists('../app/controllers/'.$url[0].'.php')){
                 $this->controller = $url[0];
                 unset($url[0]);
             }
---
+++ app/views/Register/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    <link href="../../public/css/styles.css" rel="stylesheet">
+    <title>Bonekify Register Page</title>
+</head>
+
+<body>
+    <div id="register-page">
+    <img src = "../../public/assets/logo.png">
+        <h1>Sign up for free to start<br>listening.</h1>
+</div> 
+</body>
