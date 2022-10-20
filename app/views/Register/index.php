d3f6d34dabd9101b50ffc50742286a2caaea3b3a
Saul Sayers <saulsayers@gmail.com>
2022-10-20 22:36:13 +0700

    feat: fix register page

--- app/views/Register/index.php
+++ app/views/Register/index.php
@@ -5,9 +5,26 @@
     <title>Bonekify Register Page</title>
 </head>
 
-<body>
+<body class ="center">
     <div id="register-page">
     <img src = "../../public/assets/logo.png">
         <h1>Sign up for free to start<br>listening.</h1>
-</div> 
+    </div> 
+    <div>
+  <form id = "register-form">
+    <label for="fname">Apa email kamu?</label>
+    <input type="text" id="email" name="email" placeholder="Your name..">
+
+    <label for="lname">Konfirmasi email kamu</label>
+    <input type="text" id="kemail" name="kemail" placeholder="Your last name..">
+
+    <label for="lname">Buat kata sandi</label>
+    <input type="text" id="sandi" name="sandi" placeholder="Your last name..">
+
+    <label for="lname">Siapa namamu?</label>
+    <input type="text" id="nama" name="nama" placeholder="Your last name..">
+  
+    <input type="submit" value="Submit">
+  </form>
+</div>
 </body>
