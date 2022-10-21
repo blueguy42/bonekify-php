84a650b43a235c3128e24340d987e94fe02cbbdc
Saul Sayers <saulsayers@gmail.com>
2022-10-21 09:50:37 +0700

    feat: add login

---
+++ app/views/Login/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<head>
+    <link href="../../public/css/styles.css" rel="stylesheet">
+    <title>Bonekify</title>
+</head>
+
+<body class ="center">
+    <div id="register-page">
+    <img src = "../../public/img/bonekify.png" id ="logo">
+        <h4>Untuk melanjutkan, masuk ke Spotify.</h4>
+        <hr>
+    </div> 
+    <div>
+  <form id = "register-form">
+    <label for="email-nama">Alamat email atau nama pengguna</label>
+    <input type="text" id="email-nama" name="email-nama" placeholder="Alamat email atau nama pengguna">
+
+    <label for="sandi">Kata sandi</label>
+    <input type="text" id="sandi" name="sandi" placeholder="Kata sandi">
+  
+    <input type="submit" value="Masuk">
+  </form>
+  <p> <strong>Belum punya akun?</strong> <a id = "gantiwarna" href="http://localhost:8080/public/Register">Daftar.</a>
+</div>
+</body>
--- app/views/Register/index.php
+++ app/views/Register/index.php
@@ -6,25 +6,27 @@
 </head>
 
 <body class ="center">
-    <div id="register-page">
+  <div id  = "register-page">
     <img src = "../../public/img/bonekify.png" id ="logo">
         <h1>Sign up for free to start<br>listening.</h1>
-    </div> 
-    <div>
+        <hr>
+  </div> 
+  <div>
   <form id = "register-form">
     <label for="fname">Apa email kamu?</label>
-    <input type="text" id="email" name="email" placeholder="Your name..">
+    <input type="text" id="email" name="email" placeholder="Masukkan email kamu..">
 
     <label for="lname">Konfirmasi email kamu</label>
-    <input type="text" id="kemail" name="kemail" placeholder="Your last name..">
+    <input type="text" id="kemail" name="kemail" placeholder="Masukkan lagi email kamu..">
 
     <label for="lname">Buat kata sandi</label>
-    <input type="text" id="sandi" name="sandi" placeholder="Your last name..">
+    <input type="text" id="sandi" name="sandi" placeholder="Masukkan kata sandi..">
 
     <label for="lname">Siapa namamu?</label>
-    <input type="text" id="nama" name="nama" placeholder="Your last name..">
+    <input type="text" id="nama" name="nama" placeholder="Masukkan nama profil..">
   
     <input type="submit" value="Submit">
   </form>
+  <p> <strong>Punya akun?</strong> <a id = "gantiwarna" href="http://localhost:8080/public/Login">Masuk.</a>
 </div>
 </body>
