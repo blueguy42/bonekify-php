83c7e4a991c74c6dc8df170a9061b23d5296506d
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-10-27 22:42:10 +0700

    feat: tambah album

--- app/models/album_model.php
+++ app/models/album_model.php
@@ -8,5 +8,31 @@ class album_model{
         $json = mysqli_fetch_all($result,MYSQLI_ASSOC);
         return $json;
     }
+
+    public function addAlbum($file, $judul, $penyanyi, $tanggalterbit, $genre){
+        $db = db_util::connect();
+        
+        $target_dir = "img/";
+        $target_file = $target_dir . basename($file["name"]);
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            return false;
+        }
+        
+        $check = getimagesize($file["tmp_name"]);
+        if($check == false) {
+            return false;
+        }
+
+        if(!move_uploaded_file($file["tmp_name"], $target_file)){
+            return false;
+        }
+
+        # insert cover
+        $query = "INSERT INTO Album (judul, penyanyi, total_duration, image_path, tanggal_terbit, genre) VALUES 
+        ('" . $judul . "', '" . $penyanyi . "', 0, '" . basename($file["name"]) . "', '" . $tanggalterbit . "', '" . $genre . "')";
+        return mysqli_query($db,$query);
+    }
 }
 ?>
