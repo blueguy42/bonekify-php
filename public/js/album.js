7b6338f2e1b91c1ffc24bf065c4a369a117a430e
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-10-28 13:25:11 +0700

    fix: detail album empty field check

--- public/js/album.js
+++ public/js/album.js
@@ -5,8 +5,8 @@ var modal_ganti_tanggal = document.getElementById("modal-ganti-tanggal");
 
 var btn_ganti_cover = document.getElementsByClassName("tombol-ganti-file")[0];
 var btn_ganti_judul = document.getElementById("tombol-ganti-judul");
-var btn_ganti_genre = document.getElementsByClassName("tombol-ganti-deskripsi")[0];
-var btn_ganti_tanggal = document.getElementsByClassName("tombol-ganti-deskripsi")[1];
+var btn_ganti_genre = document.getElementsByClassName("tombol-ganti-deskripsi")[1];
+var btn_ganti_tanggal = document.getElementsByClassName("tombol-ganti-deskripsi")[0];
 
 var span0 = document.getElementsByClassName("close")[0];
 var span1 = document.getElementsByClassName("close")[1];
