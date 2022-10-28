88e7fcefa8e76edfed2f231674e75c7163734215
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2022-10-28 09:16:51 +0700

    feat: add edit album

--- app/controllers/Album.php
+++ app/controllers/Album.php
@@ -13,6 +13,25 @@ class Album extends Controller{
         if(isset($_POST["hapus-lagu"])){
             $berhasil = $this->model('song_model')->hapusDariAlbum($id,$_POST["hapus-lagu"]);
         }
+        if(isset($_POST["hapus-album"])){
+            $berhasil = $this->model('song_model')->hapusAlbum($id);
+            $berhasil = $this->model('album_model')->hapusAlbum($id);
+            header('Location: ' . BASEURL . '/album');
+        }
+        if(isset($_POST["judul-baru"])){
+            $berhasil = $this->model('album_model')->gantiJudul($id,$_POST["judul-baru"]);
+        }
+        if(isset($_POST["genre-baru"])){
+            $berhasil = $this->model('album_model')->gantiGenre($id,$_POST["genre-baru"]);
+        }
+        if(isset($_POST["tanggal-baru"])){
+            $berhasil = $this->model('album_model')->gantiTanggal($id,$_POST["tanggal-baru"]);
+        }
+        if(isset($_FILES["cover-baru"])){
+            $cover_baru = $_FILES['cover-baru'];
+            $berhasil = $this->model('song_model')->gantiCover($id,$cover_baru,'all');
+            $berhasil = $this->model('album_model')->gantiCover($id,$cover_baru);
+        }
         $data['album_detail'] = $this->model('album_model')->getAlbumDetail($id);
         $data['route'] = 'detail_album';
         $data['song'] = $this->model('song_model')->getAlbumSong($id);
--- app/models/album_model.php
+++ app/models/album_model.php
@@ -42,5 +42,38 @@ class album_model{
         ('" . $judul . "', '" . $penyanyi . "', 0, '" . basename($file["name"]) . "', '" . $tanggalterbit . "', '" . $genre . "')";
         return mysqli_query($db,$query);
     }
+
+    public function gantiJudul($id,$judulbaru){
+        $db = db_util::connect();
+        $query = sprintf("UPDATE Album
+                SET Judul='%s'
+                WHERE album_id=%u;",$judulbaru,$id);
+        return mysqli_query($db,$query);
+    }
+
+    public function gantiTanggal($id,$tanggalbaru){
+        $db = db_util::connect();
+        $query = sprintf("UPDATE Album
+                SET Tanggal_terbit='%s'
+                WHERE Album_id=%u",$tanggalbaru,$id);
+        return mysqli_query($db,$query);
+    }
+
+    public function gantiGenre($id,$genrebaru){
+        $db = db_util::connect();
+        $query = sprintf("UPDATE Album
+                SET Genre='%s'
+                WHERE Album_id=%u",$genrebaru,$id);
+        return mysqli_query($db,$query);
+    }
+
+    public function gantiCover($id,$coverbaru){
+        $db = db_util::connect();
+
+        #update cover saat ini ke db
+        $query = sprintf("UPDATE Album SET Image_path='%s' WHERE album_id=%u;",basename($coverbaru["name"]),$id);
+        
+        return mysqli_query($db,$query);
+    }
 }
 ?>
