309bf18d8a029f85db8f9aa6c576b79c7ba4a7c7
Rizky Ramadhana <rizkyramadhana26@gmail.com>
2022-11-30 22:27:45 +0700

    feat: hit rest

--- app/controllers/Subscribe.php
+++ app/controllers/Subscribe.php
@@ -1,5 +1,9 @@
 <?
 class Subscribe extends Controller{
+  public function index(){
+    $this->model('rest_model')->getPenyanyi();
+  }
+
   public function add($creator_id,$subscriber_id){
     $this->model('soap_model')->addSubscription($creator_id,$subscriber_id);
     $this->model('subscription_model')->add($creator_id,$subscriber_id);
