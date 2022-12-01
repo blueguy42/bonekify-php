329e4f04e7cd22733d453e2ae5617682369e6cee
Saul Sayers <saulsayers@gmail.com>
2022-12-02 05:08:46 +0700

    feat: lagu premium page

--- app/views/LaguPremium/index.php
+++ app/views/LaguPremium/index.php
@@ -1,9 +1,18 @@
-  <div>
-    <div>Judul</div>
-</div>
-  <?foreach($data['lagupremium'] as $id => $lagu){?>
-    <div>
-      <span class='judullagupremium'><?=$lagu['Judul']?></span>
-      <span> <audio controls controlsList="nodownload noplaybackrate" id="songplayer"><source src= '<?=REST.$lagu['Audio_path']?>' > </audio> </span>
-  </div>
+<h1 id="judulsearch">Dengarkan lagu premium!</h1>
+
+<table id='list-lagu' class="col-7 col-s-5" style="font-size:20px; color:black; font-weight:bold; padding:30px">
+<thead>
+        <tr>
+          <th style="font-size:20px;padding-left:10px">Judul</th>
+          <th style="font-size:20px;text-align:center">Putar Lagu</th>
+        </tr>
+      </thead>
+<?foreach($data['lagupremium'] as $id => $lagu){?>
+    
+<tr>
+      <td style="font-size:20px;padding-left:10px;margin: 5px 5px 0px 0px" class='judullagupremium'><?=$lagu['Judul']?></td>
+      <td align="center" > <audio class="putarpremium" controls controlsList="nodownload noplaybackrate" id="songplayer"><source src= '<?=REST.$lagu['Audio_path']?>' > </audio> </td>
+      </tr>
   <?}?>
+
+  </table>
--- app/views/Penyanyi/index.php
+++ app/views/Penyanyi/index.php
@@ -23,12 +23,12 @@
       <?}} else { ?>
         <td style=" padding:20px 0px;border-bottom: 1px groove #ddd;">Not requested</td>
       <? } if(!isset($data["status"][$penyanyi['user_id']])){?>
-      <td  style="border-bottom: 1px groove #ddd;"><button id="subsbutton" style="background-color: RGBA(150,20,20,0.25);border: 1px solid green; border-radius:5px;" onclick="location.href='<?=BASEURL?>/subscribe/add/<?=$penyanyi['user_id']?>/<?=$_SESSION['user_id']?>'" type="button"> SUBSCRIBE </button></td>
+      <td  style="border-bottom: 1px groove #ddd;"><button class="subsbutton" style="background-color: RGBA(150,20,20,0.25);border: 1px solid green; border-radius:5px;" onclick="location.href='<?=BASEURL?>/subscribe/add/<?=$penyanyi['user_id']?>/<?=$_SESSION['user_id']?>'" type="button"> SUBSCRIBE </button></td>
   <?}else{?>
     <?if($data["status"][$penyanyi['user_id']]=="ACCEPTED"){?>
           <td style="border-bottom: 1px groove #ddd;"><a style ="font-size:13px;margin:0px; border: 1px solid green;background-color: RGBA(20,150,20,0.25);border-radius:5px;" href='<?=BASEURL?>/lagupremium/detail/<?=$penyanyi['user_id']?>'>Listen Songs</a></td>
         <?}else{?>
-          <td  style="border-bottom: 1px groove #ddd;"><button id="subsbutton" style="background-color: RGBA(20,20,20,0.7);border: 1px solid green; border-radius:5px;" onclick="location.href='<?=BASEURL?>/subscribe/add/<?=$penyanyi['user_id']?>/<?=$_SESSION['user_id']?>'" type="button" disabled> Listen Songs </button></td>  <? }}}?>
+          <td  style="border-bottom: 1px groove #ddd;"><button class="subsbutton" style="background-color: RGBA(20,20,20,0.7);border: 1px solid green; border-radius:5px;" onclick="location.href='<?=BASEURL?>/subscribe/add/<?=$penyanyi['user_id']?>/<?=$_SESSION['user_id']?>'" type="button" disabled> Listen Songs </button></td>  <? }}}?>
 </table>
 
 
