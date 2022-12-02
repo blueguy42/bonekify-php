f943787f1db935848a9008185003b20586efceed
Rizky Ramadhana <rizkyramadhana26@gmail.com>
2022-12-02 13:14:13 +0700

    bug: fix redirect error

--- app/controllers/Subscribe.php
+++ app/controllers/Subscribe.php
@@ -7,7 +7,7 @@ class Subscribe extends Controller{
   public function add($creator_id,$subscriber_id){
     $this->model('subscription_model')->add($creator_id,$subscriber_id);
     $this->model('soap_model')->addSubscription($creator_id,$subscriber_id);
-    header('Location: ' . BASEURL . '/penyanyi');
+    header('Location: ' . BASEURL . '/premium');
   }
 
   public function setstatus($creator_id,$subscriber_id,$status){
